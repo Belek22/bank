4d47ac6774f7fa292a6211957d0e77d119f10e75
Belek22 <belek.tatar@gmail.com>
2024-07-17 18:35:22 +0600

    updated CORS

--- project/settings.py
+++ project/settings.py
@@ -164,9 +164,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'account.User'
 
-# CORS_ORIGIN_WHITELIST = ['http://localhost:3000/']
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
 
-CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_HEADERS = [
     'accept',
