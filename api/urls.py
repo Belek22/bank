42ea1518f2954295ebc338a945115e82c0f680bd
Belek22 <belek.tatar@gmail.com>
2024-07-27 20:38:00 +0600

    updated urls

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from .views import DayOfWeekViewSet, WorkScheduleViewSet, BookingViewSet
 
@@ -10,6 +9,4 @@ router.register('bookings', BookingViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('auth/', include('api.auth.urls')),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api-token-auth/', obtain_auth_token),
 ]
