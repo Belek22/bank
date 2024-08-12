d18fc1282144bcd9607e0bf57b44cd1a36393f30
Belek22 <belek.tatar@gmail.com>
2024-08-12 14:40:16 +0600

    finish

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 from core.models import DayOfWeek, WorkSchedule, Booking
 from account.models import User
-from datetime import date, datetime
+from datetime import datetime
 import pytz
-
+from django.db import transaction
 
 class DayOfWeekSerializer(serializers.ModelSerializer):
     class Meta:
@@ -18,27 +18,20 @@ class WorkScheduleSerializer(serializers.ModelSerializer):
         read_only_fields = ['user']
 
     def validate(self, data):
-        try:
-            user = self.context['request'].user
-            schedule_date = data.get('date')
-            start_time = data.get('start_time')
-            end_time = data.get('end_time')
+        user = self.context['request'].user
+        schedule_date = data.get('date')
+        start_time = data.get('start_time')
+        end_time = data.get('end_time')
 
-            # Проверка пересечения с существующими расписаниями
-            existing_schedules = WorkSchedule.objects.filter(user=user, date=schedule_date)
-            for schedule in existing_schedules:
-                if not (end_time <= schedule.start_time or start_time >= schedule.end_time):
-                    raise serializers.ValidationError("Расписание пересекается с существующим.")
+        existing_schedules = WorkSchedule.objects.filter(user=user, date=schedule_date)
+        for schedule in existing_schedules:
+            if not (end_time <= schedule.start_time or start_time >= schedule.end_time):
+                raise serializers.ValidationError("Расписание пересекается с существующим.")
 
-            # Проверка на выходные дни
-            if schedule_date.weekday() in [5, 6]:
-                raise serializers.ValidationError("Нельзя добавлять расписание на выходные дни.")
+        if schedule_date.weekday() in [5, 6]:
+            raise serializers.ValidationError("Нельзя добавлять расписание на выходные дни.")
 
-            return data
-        except Exception as e:
-            print(f"Ошибка в валидации: {e}")
-            raise
-
+        return data
 
 
 class DateTimeFieldWithCustomFormat(serializers.DateTimeField):
@@ -47,6 +40,7 @@ class DateTimeFieldWithCustomFormat(serializers.DateTimeField):
             return datetime.strptime(value, '%d:%m:%Y %H:%M')
         except ValueError:
             raise serializers.ValidationError("Неверный формат даты и времени. Используйте формат 'День:Месяц:Год Часы:Минуты'.")
+
 
 class BookingSerializer(serializers.ModelSerializer):
     banker = serializers.SlugRelatedField(slug_field='username', queryset=User.objects.all())
@@ -67,46 +61,36 @@ class BookingSerializer(serializers.ModelSerializer):
 
         booking_date = booking_end_time.date()
         utc = pytz.UTC
-
-        # Преобразование в offset-aware даты и времени
         now = utc.localize(datetime.now())
+
         if booking_start_time:
             booking_start_time = utc.localize(booking_start_time)
         booking_end_time = utc.localize(booking_end_time)
 
-        # Проверка на прошедшие даты
         if booking_start_time and booking_start_time < now:
             raise serializers.ValidationError("Нельзя бронировать прошедшие даты.")
         if booking_end_time < now:
             raise serializers.ValidationError("Нельзя бронировать прошедшие даты.")
 
-        # Проверка на выходные дни
         if booking_date.weekday() in [5, 6]:
-            raise serializers.ValidationError("К сожалению, банкир в этот день отдыхает.")
+            raise serializers.ValidationError("К сожалению, сегодня не рабочий день.")
 
-        # Проверка на рабочее расписание банкира
         work_schedule = WorkSchedule.objects.filter(user=banker, date=booking_date).first()
         if not work_schedule:
             raise serializers.ValidationError("В этот день банкир не работает.")
 
-        # Установка времени начала бронирования по умолчанию, если не указано
         if not booking_start_time:
             booking_start_time = datetime.combine(booking_date, work_schedule.start_time)
             booking_start_time = utc.localize(booking_start_time)
             data['booking_start_time'] = booking_start_time
 
-        # Преобразование времени начала и конца работы банкира в offset-aware
-        work_start_time = datetime.combine(booking_date, work_schedule.start_time)
-        work_end_time = datetime.combine(booking_date, work_schedule.end_time)
-        work_start_time = utc.localize(work_start_time)
-        work_end_time = utc.localize(work_end_time)
+        work_start_time = utc.localize(datetime.combine(booking_date, work_schedule.start_time))
+        work_end_time = utc.localize(datetime.combine(booking_date, work_schedule.end_time))
 
-        # Проверка на соответствие времени бронирования рабочему времени банкира
         if not (work_start_time <= booking_start_time <= work_end_time) or \
                 not (work_start_time <= booking_end_time <= work_end_time):
             raise serializers.ValidationError("Время бронирования не соответствует рабочему времени банкира.")
 
-        # Проверка пересечения с существующими бронированиями
         overlapping_bookings = Booking.objects.filter(
             banker=banker,
             booking_start_time__lt=booking_end_time,
@@ -117,6 +101,21 @@ class BookingSerializer(serializers.ModelSerializer):
 
         return data
 
+    @transaction.atomic
     def create(self, validated_data):
         validated_data['client'] = self.context['request'].user
+
+        banker = validated_data['banker']
+        booking_start_time = validated_data['booking_start_time']
+        booking_end_time = validated_data['booking_end_time']
+
+        overlapping_bookings = Booking.objects.select_for_update().filter(
+            banker=banker,
+            booking_start_time__lt=booking_end_time,
+            booking_end_time__gt=booking_start_time
+        )
+
+        if overlapping_bookings.exists():
+            raise serializers.ValidationError("Банкир уже забронирован на это время.")
+
         return super().create(validated_data)
